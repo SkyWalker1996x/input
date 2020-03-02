dc787919fb4d6d131151f2344270cd4a9efde774
SkyWalker1996x <chehuta1996@gmail.com>
2020-03-02 21:07:39 +0200

    made basic structure

--- src/char-component/char-components.js
+++ src/char-component/char-components.js
@@ -1,23 +1,15 @@
 import React from "react";
 import './char-component.css'
 
-const CharComponent = ({chars, onDeleteChar}) => {
-
-    const elements = chars.map((item) => {
-        const {id, label} = item;
-        return (
-            <div
-                className="char"
-                key={id}
-                onClick={() => onDeleteChar(chars, id)}>
-                {label}
-            </div>
-        )
-    });
+const CharComponent = () => {
 
     return (
         <div className='char-wrapper'>
-            {elements}
+
+            <div className="char">
+                char
+            </div>
+
         </div>
     )
 };
--- src/validation-component/validation-component.js
+++ src/validation-component/validation-component.js
@@ -1,14 +1,8 @@
 import React from "react";
 
-const ValidationComponent = ({inputLength}) => {
-
-  const shortText = 'Text too short';
-  const longText = 'Text long enough';
-
-  const label = inputLength > 5 ? longText : shortText;
-
-  return (
-      <p>{label}</p>
+const ValidationComponent = () => {
+    return (
+      <p>validation component</p>
   )
 };
 
